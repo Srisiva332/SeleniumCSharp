6e21cf046956a56c17dfdf391dec9265ca8e06d0
Siva <srisiva332@gmail.com>
2020-03-22 17:41:09 +0530

    First Commit

---
+++ SeleniumCSharpFramework/Configurations/EnvironmentData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpFramework.Configurations
+{
+    public class EnvironmentData
+    {
+        public string LoginUrl { get; set; }
+    }
+}
--- SeleniumCSharpFramework/Tests/LoginTests.cs
+++ SeleniumCSharpFramework/Tests/LoginTests.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using SeleniumCSharpFramework.Configurations;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SeleniumCSharpFramework.Tests
 {
-    class LoginTests
+    [TestFixture("Environments/qa.json")]
+    public class LoginTests
     {
+        readonly EnvironmentData environmentData;
+        readonly string configfile;
+
+        public LoginTests(string configfile)
+        {
+            this.configfile = configfile;
+            var config = this.InitConfiguration();
+            environmentData = config.GetSection("EnvironmentData").Get<EnvironmentData>();
+        }
+
+
+        [Test]
+        public void SampleTest()
+        {
+            Console.WriteLine(environmentData.LoginUrl);
+        }
+
+        internal IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile(configfile)
+                .Build();
+            return config;
+        }
+
     }
 }
